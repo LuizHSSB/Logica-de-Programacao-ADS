0d207c69c99e1368785b373d60d7fd6121ed93ae
LuizHSSB <luizhenrique.macieldeoliveira@gmail.com>
2024-05-09 17:02:56 -0300

    Lista 3

---
+++ Lista 3/script/atividades.js
@@ -0,0 +1,111 @@
+$("#atividade1").on("click",function(){
+    //Questões visuais (Css/BootStrap)
+    $("#atividades").removeClass("align-items-center")
+    $("#atividades").removeClass("h-100vh")
+    $("#atividades").addClass("mt-4")
+    $("#att1").removeClass("d-none")
+    $("#att1").addClass("d-flex")
+    $("#att2").removeClass("d-flex")
+    $("#att2").addClass("d-none")
+    $("#att3").removeClass("d-flex")
+    $("#att3").addClass("d-none")
+    $("#att4").removeClass("d-flex")
+    $("#att4").addClass("d-none")
+    $("#att5").removeClass("d-flex")
+    $("#att5").addClass("d-none")
+
+    //Inicio da resolução
+
+    let pessoas = []
+
+
+    //pessoas.push("Pessoa1","Pessoa2")
+
+    $("#att1").html("<p>Oi</p>")
+
+
+});
+
+$("#atividade2").on("click",function(){
+    //Questões visuais (Css/BootStrap)
+    $("#atividades").removeClass("align-items-center")
+    $("#atividades").removeClass("h-100vh")
+    $("#atividades").addClass("mt-4")
+    $("#att1").removeClass("d-flex")
+    $("#att1").addClass("d-none")
+    $("#att2").removeClass("d-none")
+    $("#att2").addClass("d-flex")
+    $("#att3").removeClass("d-flex")
+    $("#att3").addClass("d-none")
+    $("#att4").removeClass("d-flex")
+    $("#att4").addClass("d-none")
+    $("#att5").removeClass("d-flex")
+    $("#att5").addClass("d-none")
+
+    //Inicio da resolução
+
+    $("#att2").html("")
+});
+
+$("#atividade3").on("click",function(){
+    //Questões visuais (Css/BootStrap)
+    $("#atividades").removeClass("align-items-center")
+    $("#atividades").removeClass("h-100vh")
+    $("#atividades").addClass("mt-4")
+    $("#att1").removeClass("d-flex")
+    $("#att1").addClass("d-none")
+    $("#att2").removeClass("d-flex")
+    $("#att2").addClass("d-none")
+    $("#att3").removeClass("d-none")
+    $("#att3").addClass("d-flex")
+    $("#att4").removeClass("d-flex")
+    $("#att4").addClass("d-none")
+    $("#att5").removeClass("d-flex")
+    $("#att5").addClass("d-none")
+
+    //Inicio da resolução
+
+    $("#att3").html("")
+});
+
+$("#atividade4").on("click",function(){
+    //Questões visuais (Css/BootStrap)
+    $("#atividades").removeClass("align-items-center")
+    $("#atividades").removeClass("h-100vh")
+    $("#atividades").addClass("mt-4")
+    $("#att1").removeClass("d-flex")
+    $("#att1").addClass("d-none")
+    $("#att2").removeClass("d-flex")
+    $("#att2").addClass("d-none")
+    $("#att3").removeClass("d-flex")
+    $("#att3").addClass("d-none")
+    $("#att4").removeClass("d-none")
+    $("#att4").addClass("d-flex")
+    $("#att5").removeClass("d-flex")
+    $("#att5").addClass("d-none")
+
+    //Inicio da resolução
+
+    $("#att4").html("")
+});
+
+$("#atividade5").on("click",function(){
+    //Questões visuais (Css/BootStrap)
+    $("#atividades").removeClass("align-items-center")
+    $("#atividades").removeClass("h-100vh")
+    $("#atividades").addClass("mt-4")
+    $("#att1").removeClass("d-flex")
+    $("#att1").addClass("d-none")
+    $("#att2").removeClass("d-flex")
+    $("#att2").addClass("d-none")
+    $("#att3").removeClass("d-flex")
+    $("#att3").addClass("d-none")
+    $("#att4").removeClass("d-flex")
+    $("#att4").addClass("d-none")
+    $("#att5").removeClass("d-none")
+    $("#att5").addClass("d-flex")
+
+    //Inicio da resolução
+
+    $("#att5").html("")
+});
